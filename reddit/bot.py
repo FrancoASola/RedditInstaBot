1b069879e718f8bc8ae0600cb860f65d6a070b96
Franco Sola <francosola@pop-os.localdomain>
2020-05-19 12:41:04 -0600

    development-pictures saving to respective directories from reddit

--- reddit/bot.py
+++ reddit/bot.py
@@ -1,4 +1,4 @@
-import praw, re, requests, time, os
+import praw, re, requests, time, os, json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,8 +12,37 @@ reddit = praw.Reddit(client_id=client_id,
 
 subreddit = 'battlestations'
 
+print()
+
 hot_posts = reddit.subreddit(subreddit).hot(limit=10)
 
 for post in hot_posts:
-    print(post.url)
+    url = post.url
+    print(url)
 
+
+    file_name = url.split('/')
+    if len(file_name) == 0:
+        file_name = re.findall("/(.*?)", url)
+    file_name = file_name[-1]
+    if "." not in file_name:
+        file_name += ".jpg" 
+    
+    if file_name != ".jpg":
+        # Make new directory and save image
+        r = requests.get(url)
+        
+        path = "../posts/"+file_name[:-4]
+         
+        try:
+            os.mkdir(path)
+        except:
+            print(f"Creattion of the directory failed ({url})")
+
+        with open(os.path.join(path, file_name), 'wb') as f:
+            f.write(r.content)
+
+        #Save post info into directory
+        author = post.author
+        title = post.title
+        print(author, title)
